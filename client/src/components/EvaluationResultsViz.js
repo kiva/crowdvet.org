2bd85e16aa64b2887178dabb2e7fdf7dd2926054
walter <buczacka.walter@gmail.com>
2018-07-16 10:12:14 -0300

    add results visualization component

---
+++ client/src/components/EvaluationResultsViz.js
@@ -0,0 +1,248 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Field, reduxForm } from "redux-form";
+import * as actions from "../actions";
+import SubMenu from "./SubMenu";
+import TopMenu from "./TopMenu";
+import KivaMessage from "./KivaMessage";
+import _ from "lodash";
+import moment from "moment";
+import utils from "./utils";
+import { Sparklines,SparklinesLine, SparklinesSpots, SparklinesCurve, SparklinesReferenceLine  } from 'react-sparklines';
+import LineChart from './Chart'
+
+class EvaluationResults extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      menu: { 1: "Review", 2: "Evaluation", 3: "Results" },
+      active: 1
+    };
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.props.fetchEnterprise(this.props.match.params.id);
+    this.props.fetchUserEvaluations();
+    this.props.fetchQuestions();
+    this.props.fetchOfficialEvaluations();
+    this.props.fetchCrowdVotes();
+  }
+
+  onSubMenuChange = (menu, active) => {
+    this.setState({ menu, active });
+  };
+
+  renderTable(result, evaluationResult) {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th />
+              <th>Score</th>
+              <th>Accuracy</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <th>This Enterprise</th>
+              <td ><h4>{evaluationResult ? evaluationResult.Score : ""}</h4></td>
+              <td><h4>{evaluationResult ? evaluationResult.Accuracy : ""}%</h4></td>
+            </tr>
+            <tr>
+              <th>Overall</th>
+              <td><h4>{result ? result.GeneralScore : ""}</h4></td>
+              <td><h4>{result ? result.GeneralAccuracy : ""}%</h4></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  renderSparkLine(data) {
+    return (
+      <Sparklines data={data}>
+        <SparklinesCurve></SparklinesCurve>
+        <SparklinesReferenceLine style={{stroke: "#61a63a", strokeDasharray: '2, 2'}} type="avg" />
+      </Sparklines>
+    )
+  }
+
+  render() {
+    const {
+      auth,
+      questions,
+      enterprise,
+      officialEvaluation,
+      officialEvaluations,
+      evaluation,
+      evaluations
+    } = this.props;
+    // redirect user if not logged
+    if (!auth) return null;
+    if (!enterprise) return null;
+    if (!officialEvaluation) return null;
+
+    const result = utils.getOverallResults(evaluations, officialEvaluations);
+
+    const answer = utils.getValues(evaluation.Votes, "Answer");
+    const officialAnswer = utils.getValues( officialEvaluation.Votes, "Answer");
+
+    const evaluationResult = utils.getScoreAndAccuracy(
+      utils.getValues(answer, "score"),
+      utils.getValues(officialAnswer, "score")
+    );
+
+    this.sector = enterprise.Sector ? enterprise.Sector.name : "Water";
+    const imgName = `/sectors/${this.sector}.jpg`;
+    const message = `KIVA ${officialEvaluation.status.toUpperCase()} THIS LOAN`;
+    let background;
+    switch (officialEvaluation.status) {
+      case "Approved":
+        background = "background-approved"
+        break;
+        case "Declined":
+        background = "background-declined"
+    }
+    return (
+      <div>
+        <TopMenu onSubMenuChange={this.onSubMenuChange} />
+        <div style={{ marginBottom: "-25px" }}>
+          <div className="row">
+            <div className="col s12 center img-header">
+              <h2 id="title-img" className=" center">
+                {enterprise.name}
+              </h2>
+            </div>
+            <img
+              className="responsive-img img-pic"
+              src={imgName}
+              width="100%"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="row flow-text dashboard">
+          <SubMenu menu={this.state.menu} />
+        </div>
+        <div className="container">
+          <div className="row flow-text center">
+            <h3 className="col s12">Evaluation Results</h3>
+          </div>
+          <KivaMessage message={message} background={background}/>
+          <div className="row">
+            {this.renderTable(result, evaluationResult)}
+          </div>
+
+          <div className="row">{this.renderResults()}</div>
+          <div className="row">
+            <div className="col s6">
+              <Link to={"/user"} className="btn button-large btn-results">
+                Previous Page
+              </Link>
+            </div>
+            <div className="col s6">
+              <Link to={"/user"} className="btn button-large btn-results">
+                Exit
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderResults() {
+    const content = _.map(this.props.questions, question => {
+      console.log(question);
+      return (
+        <div className="row">
+          <div className="question">{question.text}</div>
+          {this.renderAnswers(question, question.Answers)}
+        </div>
+      );
+    });
+    return content;
+  }
+
+  renderAnswers(question, answers) {
+    const { evaluation } = this.props;
+    const votes = _.get(evaluation, "Votes");
+    let text = "";
+    const content = _.map(answers, answer => {
+      const v = _.mapKeys(votes, "question_id");
+      const checked =
+        answer.id == _.get(v[question.id], "answer_id") ? true : false;
+      answer.id == _.get(v[question.id], "answer_id")
+        ? (text = answer.text)
+        : null;
+      return (
+        <div>
+          <div className="col s2">
+            <p>
+              <label>
+                <input
+                  name={question.id.toString()}
+                  type="radio"
+                  checked={checked}
+                  disabled={true}
+                />
+                <span id="radio-text">{answer.score}</span>
+              </label>
+            </p>
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div>
+        {content}
+        <div className="col s12 answer-result">
+          <p>{text}</p>
+        </div>
+        <div className="col s12">
+        <LineChart />
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(
+  { auth, enterprises, questions, evaluations, officialEvaluations, crowdVotes },
+  ownProps
+) {
+  console.log(questions, "con q")
+  console.log(crowdVotes, "en votes")
+  const initial = _.reduce(questions, (result, q) => {
+      const withCount = _.map(q.Answers, answer => { return {...answer, count:0} })
+      const answers = _.mapKeys(withCount, "id");
+      return {...result, [q.id]: answers }
+  }, {})
+
+  const r = _.reduce(crowdVotes, (result, evaluation) => {
+      if (evaluation.enterprise_id === ownProps.match.params.id) {
+          _.map(evaluation.Votes, vote => {
+            result[vote.question_id][vote.answer_id].count = result[vote.question_id][vote.answer_id].count + 1;
+          })
+      }
+      return result;
+  }, initial)
+
+console.log(r, "en result")
+  return {
+    auth,
+    enterprise: enterprises[ownProps.match.params.id],
+    evaluation: evaluations[ownProps.match.params.id],
+    questions,
+    evaluations,
+    officialEvaluations,
+    officialEvaluation: officialEvaluations[ownProps.match.params.id],
+    crowdVotes: crowdVotes[ownProps.match.params.id],
+  };
+}
+export default connect(mapStateToProps, actions)(EvaluationResults);
