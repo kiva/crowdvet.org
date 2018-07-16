64f16b626dfbc3c888a54d1b4808643646b8be35
walter <buczacka.walter@gmail.com>
2018-07-16 10:16:59 -0300

    add top menu

---
+++ client/src/components/TopMenu.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as actions from '../actions';
+import _ from 'lodash';
+
+class TopMenu extends Component {
+  render() {
+    return (
+      <div className="row center-align dashboard">
+        <div className="col s4 top-menu">
+          <Link to={'/user'} className="btn btn-flat btn-large" >
+            Profile
+          </Link>
+        </div>
+        <div className="col s4 top-menu">
+          <Link to={"/vet/enterprises"} className="btn btn-flat btn-large">
+            Vet Enterprises
+          </Link>
+        </div>
+        <div className="col s4 top-menu">
+          <button className="btn btn-flat btn-large">Training</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TopMenu;
