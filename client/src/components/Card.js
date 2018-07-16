03e3681a09cd3ddb48846f8de45c86d8b33fd6e7
walter <buczacka.walter@gmail.com>
2018-07-16 10:19:48 -0300

    implement card component for application

---
+++ client/src/components/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Card = ({ name, description, mainText }) => {
+  return (
+    <div className="col s12 m4 l4 card-review">
+      <div className="card-panel teal">
+        <div className="title">{name}</div>
+        <span>
+          <h3>{mainText}</h3>
+          <small>{description}</small>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
