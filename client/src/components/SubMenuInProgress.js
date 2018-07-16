1c0d56dd9f6c2ed505a8bf2b9b9c986140442a2d
walter <buczacka.walter@gmail.com>
2018-07-16 10:16:30 -0300

    add submenu

---
+++ client/src/components/SubMenuInProgress.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import * as actions from '../actions';
+import _ from 'lodash';
+
+class SubMenuInprogress extends Component {
+
+  onHandleClickInProgress() {
+    this.props.onSubMenuChange(
+      {
+        1: 'Reviews In Progress',
+        2: 'Enterprises Suggested for You'
+      },
+      1
+    )
+  }
+
+  onHandleClickSuggested() {
+    this.props.onSubMenuChange(
+      {
+        1: 'Reviews In Progress',
+        2: 'Enterprises Suggested for You'
+      },
+      2
+    )
+  }
+
+  render() {
+      const menu = (
+        <div>
+        <div className='col s6 center'>
+        <button onClick={this.onHandleClickInProgress.bind(this)} className="btn btn-flat btn-large dashboard-item">
+          Reviews In Progress
+        </button>
+        </div>
+        <div className="col s6 center">
+        <button onClick={this.onHandleClickSuggested.bind(this)} className="btn btn-flat btn-large dashboard-item">
+          Enterprises Suggested for You
+        </button>
+        </div>
+        </div>
+      );
+
+    return menu;
+  }
+}
+
+export default SubMenuInprogress;
