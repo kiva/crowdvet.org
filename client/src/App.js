37578f7dfa91bab8f4f11ff0f2d30ae90e7a3e36
walter <buczacka.walter@gmail.com>
2018-07-16 09:26:44 -0300

    add route components to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,44 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Landing from "./components/Landing";
+import UserProfile from "./components/Profile";
+import ApplicationShow from "./components/ApplicationShow";
+import ApplicationEvaluate from "./components/Evaluation";
+import Admin from "./components/Admin";
+import VetEnterprises from "./components/VetEnterprises";
+import EvaluationResults from "./components/EvaluationResults";
+import EvaluationResultsViz from "./components/EvaluationResultsViz";
+import LineChart from "./components/Chart";
+import * as actions from "./actions";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Landing from './components/Landing';
-
-import './App.css';
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
   render() {
     return (
       <div>
-        <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Footer />
-        </div>
-        </BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/users/evaluations/results/:id" component={EvaluationResults} />
+          <Route exact path="/evaluations/results/:id" component={EvaluationResultsViz} />
+          <Route exact path="/vet/enterprises" component={VetEnterprises} />
+          <Route exact path="/user" component={UserProfile} />
+          <Route exact path="/application/:id" component={ApplicationShow}
+          />
+          <Route path="/users/evaluations/:id" component={ApplicationEvaluate}
+          />
+          <Route path="/" component={Landing} />
+        </Switch>
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
