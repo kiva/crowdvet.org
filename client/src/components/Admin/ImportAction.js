7e9075868a10e68a5ed401e0b24e2f9243c493e5
walter <buczacka.walter@gmail.com>
2018-07-29 17:41:58 -0300

    add import action component

---
+++ client/src/components/Admin/ImportAction.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { showNotification as showNotificationAction } from 'react-admin';
+import { push as pushAction } from 'react-router-redux';
+
+class ImportAction extends Component {
+  componentDidMount = () => {
+    const { resource, basePath, selectedIds, onExit, pushAction } = this.props;
+    this.importEnterprises(selectedIds);
+    onExit();
+  };
+
+  importEnterprises(ids) {
+    axios
+      .post('/api/enterprises', { ids })
+      .then(res => {
+        this.props.showNotificationAction('Applications imported');
+      })
+      .catch(err => {
+        this.props.showNotificationAction(
+          'An error ocurred while importing applicattions',
+          'warning'
+        );
+      });
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default connect(undefined, { showNotificationAction, pushAction })(
+  ImportAction
+);
