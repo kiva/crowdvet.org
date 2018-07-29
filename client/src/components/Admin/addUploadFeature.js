c903c13f7ad205cda7e32839ce941a400e417c86
walter <buczacka.walter@gmail.com>
2018-07-29 17:46:59 -0300

    add upload files implementation

---
+++ client/src/components/Admin/addUploadFeature.js
@@ -0,0 +1,44 @@
+
+const convertFileToBase64 = file =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file.rawFile);
+
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = reject;
+  });
+
+const addUploadFeature = requestHandler => (type, resource, params) => {
+  if (type === "UPDATE" && resource === "enterprises") {
+
+    if (params.data.pictures) {
+      const formerPictures = params.data.pictures.filter(
+        p => !(p.rawFile instanceof File)
+      );
+      const newPictures = params.data.pictures.filter(
+        p => p.rawFile instanceof File
+      );
+
+      return Promise.all(newPictures.map(convertFileToBase64))
+        .then(base64Pictures =>
+          base64Pictures.map(picture64 => ({
+            src: picture64,
+            title: `${params.data.title}`
+          }))
+        )
+        .then(transformedNewPictures =>
+          requestHandler(type, resource, {
+            ...params,
+            data: {
+              ...params.data,
+              pictures: [...transformedNewPictures, ...formerPictures]
+            }
+          })
+        );
+    }
+  }
+  // for other request types and reources, fall back to the defautl request handler
+  return requestHandler(type, resource, params);
+};
+
+export default addUploadFeature;
