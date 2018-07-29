4601e3c64611af130faa6cd98ed243ce33256cd7
walter <buczacka.walter@gmail.com>
2018-07-29 17:52:21 -0300

    add comments reducer

---
+++ client/src/reducer/commentsReducer.js
@@ -0,0 +1,24 @@
+import { FETCH_COMMENT, FETCH_COMMENTS, FETCH_COMMENT_REPLY } from "../actions/types";
+import _ from "lodash";
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case FETCH_COMMENT:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_COMMENTS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_COMMENT_REPLY:
+      const reply = action.payload;
+      const comment = state[action.payload.comment_id];
+
+      return {
+        ...state,
+        [action.payload.comment_id]: {
+          ...comment,
+          Replies: comment.Replies ? comment.Replies.concat(reply) : [{...reply}]
+        }
+      };
+    default:
+      return state;
+  }
+}
