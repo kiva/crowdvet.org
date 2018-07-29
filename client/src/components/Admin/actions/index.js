3b9b27820c8f7097be4cc89fa0ea2c017aeee435
walter <buczacka.walter@gmail.com>
2018-07-29 17:46:43 -0300

    add import action implementation

---
+++ client/src/components/Admin/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { showNotification as showNotificationAction } from 'react-admin';
+import { push } from 'react-router-redux';
+
+export const importEnterprises = (ids) => {
+  axios.post('/api/enterprises', { ids })
+  .then(res => {
+    showNotificationAction('Applications imported');
+    push('/applications');
+  })
+  .catch(err => {
+    showNotificationAction(
+      'An error ocurred while importing applicattions',
+      'warning'
+    );
+  });
+};
