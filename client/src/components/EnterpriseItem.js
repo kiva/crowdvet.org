7d3c6550683a59df09a51c2b899d15d6cecb4ed0
walter <buczacka.walter@gmail.com>
2018-07-29 16:59:03 -0300

    adding carousel for open enterprises

--- client/src/components/EnterpriseItem.js
+++ client/src/components/EnterpriseItem.js
@@ -1,30 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import moment from "moment";
+import _ from "lodash";
+import { Carousel } from "react-materialize";
 
-const EnterpriseItem = ({ enterprise}) => {
-  return (
-    <div className="card horizontal gray-background">
-      <div className="card-stacked">
-        <div className="card-content item-name left-align">
-          {renderName(enterprise.name)}
+class EnterpriseItem extends Component {
+  renderImages(images) {
+    const content = _.map(images, image => image.url);
+    return (
+      <Carousel
+        className="open-enterprises"
+        carouselId={this.props.enterprise.id}
+        options={{ fullWidth: true, indicators: true }}
+        images={content}
+      />
+    );
+  }
+
+  render() {
+    const { enterprise } = this.props;
+    return (
+      <div className="card horizontal gray-background">
+        <div className="card-stacked">
+          <div className="card-content item-name left-align">
+            {renderName(enterprise.name)}
+          </div>
+          <div className="item-date left-align">
+            Ends on {moment(enterprise.endDate).format("MM-DD-YYYY")}
+          </div>
+          <div className="card-content item-description">
+            {enterprise.description.substring(0, 130)}...
+          </div>
+          <div className="right-align">
+            <Link to={`/application/${enterprise.id}`} className="btn">
+              review application
+            </Link>
+          </div>
         </div>
-        <div className="item-date left-align">Ends on {moment(enterprise.endDate).format("MM-DD-YYYY")}</div>
-        <div className="card-content item-description">
-          {enterprise.description.substring(0,130)}...
-        </div>
-        <div className="right-align">
-          <Link to={`/application/${enterprise.id}`} className="btn">review application</Link>
+        <div className="crop">
+          {!_.isEmpty(enterprise.Images) ? (
+            this.renderImages(enterprise.Images)
+          ) : (
+            <img src={enterprise.image1} />
+          )}
         </div>
       </div>
-      <div className="crop" >
-        <img src={enterprise.image1}/>
-      </div>
-    </div>
-  );
-};
-
-const renderName = (name) => {
-  return name.length > 15 ? `${name.substring(0,15)}...` : name
+    );
+  }
 }
+
+const renderName = name => {
+  return name.length > 15 ? `${name.substring(0, 15)}...` : name;
+};
 export default EnterpriseItem;
--- client/src/components/Enterprises.js
+++ client/src/components/Enterprises.js
@@ -5,9 +5,15 @@ import * as actions from '../actions';
 import _ from 'lodash';
 import EnterpriseItem from './EnterpriseItem'
 import './Enterprises.css';
+import Filter from './Filter'
+import Sort from './Sort'
 
 class EnterpisesList extends Component {
 
+  constructor(props) {
+    super(props);
+    this.filters = {sector_id:[], country_id:[], sort:""};
+  }
   renderEnterprises(enterprises) {
     const enterprisesItems = _.map(enterprises, (enterprise) => {
       return <EnterpriseItem key={ enterprise.id } userId={this.props.userId} enterprise={ enterprise } />
@@ -15,12 +21,27 @@ class EnterpisesList extends Component {
     return enterprisesItems;
   }
 
+  filter(type, id, value) {
+    if(value) {
+      this.filters = {...this.filters, [type]:[...this.filters[type], id] }
+    } else {
+      this.filters = {...this.filters, [type]:[ ..._.filter(this.filters[type], (item => item !== id))] }
+    }
+    this.props.fetchEnterprises(this.filters);
+  }
+
+  sort(by) {
+    this.filters = {...this.filters,  ["sort"]: by };
+    this.props.fetchEnterprises(this.filters);
+  }
+
   render() {
     return (
       <div className="container">
       <div className="row text-flow center">
         <p className="col s12 gray-background font-title">All Enterprises Open For Review</p>
-        <div className="col s12 m3 l3">Side Bar</div>
+        <div className="col s12 m3 offset-m9"><Sort filter={this.sort.bind(this)} /></div>
+        <div className="col s12 m3 l3"><Filter filter={this.filter.bind(this)} sectors={this.props.sectors} countries={this.props.countries} /></div>
         <div className="col s12 m9 l9">
           {this.renderEnterprises(this.props.enterprises)}
         </div>
@@ -29,5 +50,4 @@ class EnterpisesList extends Component {
     );
   }
 }
-
-export default EnterpisesList;
+export default connect(null, actions)(EnterpisesList);
