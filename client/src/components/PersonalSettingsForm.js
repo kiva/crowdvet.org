efbba75aa6e6c097480859de07c69e7788ae30e7
walter <buczacka.walter@gmail.com>
2018-07-29 17:11:05 -0300

    add personal settings form

---
+++ client/src/components/PersonalSettingsForm.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Field, reduxForm } from 'redux-form'
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import _ from "lodash";
+import "./Settings.css"
+
+class PersonalForm extends Component {
+
+  componentDidMount() {
+    window.jQuery(document).ready(function() {
+      window.jQuery(".collapsible.personal").collapsible();
+    });
+  }
+
+  submitForm(values) {
+    this.props.updateUserSettings(values)
+  }
+
+  renderSectors(sectors) {
+    return _.map(sectors, sector => {
+      return <Field key={sector.id} text={sector.name} name={`${sector.id.toString()}`} component={this.renderCheckBox} />
+    })
+  }
+
+  renderCheckBox = (field) => (
+    <div className="col s12 m4">
+      <label>
+      <input {...field.input} type="checkbox" checked={field.input.value ? "checked": ""}/>
+      <span>{field.text}</span>
+      {field.meta.touched && field.meta.error &&
+       <span className="error">{field.meta.error}</span>}
+       </label>
+    </div>
+  )
+
+  render() {
+    const { handleSubmit, sectors } = this.props;
+    return (
+      <div className="row">
+        <div className="col s12 m10 offset-m1">
+          <ul className="collapsible personal">
+            <li>
+              <div className="collapsible-header">
+                <h4 className="col s12 center">Personal Settings</h4>
+              </div>
+              <div className="collapsible-body">
+                <form onSubmit={handleSubmit(this.submitForm.bind(this))}>
+                  <div className="row">
+                    <div className="col s12 m6">
+                      <div className="row">
+                        <div className="col s12"><h4>About You</h4></div>
+                      </div>
+                      <Field name="name" component="input" type="text" />
+                       <label htmlFor="name">Name</label>
+                      <Field className="validate" name="email" component="input" type="email" />
+                       <label htmlFor="email">Email</label>
+                    </div>
+                    <div className="col s12 m6">
+                      <div className="row">
+                        <div className="col s12"><h4>Sectors You are Interested In</h4></div>
+                      </div>
+                        {this.renderSectors(sectors)}
+                      </div>
+                    <div className="col s12 m6 offset-m3 center">
+                      <button className="btn button-large">Save Changes</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+function getInitialValues(auth) {
+  const sectors = _.reduce(auth.UsersSectors, (result, value) => {
+    result[value.sector_id] = true;
+    return result
+  } ,{})
+
+  return {
+    name:auth.name,
+    email: auth.email,
+    ...sectors
+  }
+}
+export default connect( ({auth} ) => ({initialValues: getInitialValues(auth)}),
+ actions)(
+  reduxForm({ form: "personalform"})(PersonalForm)
+);
