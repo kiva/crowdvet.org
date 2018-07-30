9e7bbbdc72fca5c15aebd93ddc2c58812068f2c8
walter <buczacka.walter@gmail.com>
2018-07-30 08:25:06 -0300

    add env vars to prod keys

--- config/prod.js
+++ config/prod.js
@@ -2,12 +2,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  database: '',
-  username: '',
-  password: '',
-  dialect: '',
-  storage: "",
-  jwtSecret:"",
+  databaseURL: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN,
   API_KEY = process.env.API_KEY;
