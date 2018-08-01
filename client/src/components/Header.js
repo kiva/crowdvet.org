b786536b4bb30859b323ec7b020d25f1c4ea5ed8
walter <buczacka.walter@gmail.com>
2018-08-01 08:47:11 -0300

    add link to logout dropdown

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
             <Dropdown
               trigger={
                 <div>
-                  <i className="material-icons right">arrow_drop_down</i>
+                  <a><i className="material-icons right">arrow_drop_down</i></a>
                 </div>
               }
             >
