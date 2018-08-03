40774dfaac2717ee6acb992040693c62dda19fac
walter <buczacka.walter@gmail.com>
2018-08-03 14:29:02 -0300

    update users routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const requireLogin = require("../middlewares/requireLogin");
+const passport = require("passport");
 const _ = require("lodash");
 
 module.exports = app => {
@@ -12,12 +13,15 @@ module.exports = app => {
     Sectors,
     Users,
     Images,
-    UsersSectors
+    UsersSectors,
+    Countries,
+    Comments
   } = app.datasource.models.Enterprises.model;
 
   app.get("/api/users", async (req, res) => {
     try {
-      const result = await Users.findAll();
+      const { admin } = req.query;
+      const result = await Users.findAll({where: {admin: admin || false}});
       return res
         .status(200)
         .set("x-Total-Count", result.length)
@@ -27,31 +31,40 @@ module.exports = app => {
     }
   });
 
-  app.get("/api/users/enterprises", async (req, res) => {
+  app.get("/api/users/enterprises", requireLogin, async (req, res) => {
     try {
       const { filter, sort } = req.query;
       let filters = { published: true }
+      let order = ["name"];
+
       if (filter) {
          const { sector_id, country_id}  = JSON.parse(filter)
          filters = !_.isEmpty(sector_id) ? { ...filters, sector_id} : { ...filters }
          filters = !_.isEmpty(country_id) ? { ...filters, country_id} : { ...filters }
       }
-      if (sort) {
-         const { sector_id, country_id}  = JSON.parse(filter)
-         filters = !_.isEmpty(sector_id) ? { ...filters, sector_id} : { ...filters }
-         filters = !_.isEmpty(country_id) ? { ...filters, country_id} : { ...filters }
-      }
 
-      console.log("--------")
-      console.log(filters)
-      console.log("--------")
+      if (sort === "country") {
+         order = [Countries,  "name", "ASC"]
+      }
+      if (sort === "sectors") {
+         order = [Sectors,  "name", "ASC"]
+      }
+      if (sort === "sectors") {
+         order = [Sectors,  "name", "ASC"]
+      }
+      if (sort === "endDate") {
+         order = ["endDate", "DESC"]
+      }
 
       const result = await Enterprises.findAll({
         where: {...filters},
+        order: [[...order]],
         include: [
           { model: Evaluations, as: "Evaluations" },
           { model: Images },
-          { model: Sectors }
+          { model: Sectors },
+          { model: Comments },
+          { model: Countries }
         ]
       });
 
@@ -79,17 +92,6 @@ module.exports = app => {
     }
   });
 
-  app.get("/api/users/votes", async (req, res) => {
-    try {
-      const result = await Evaluations.findAll({
-        include: [{ model: Votes }]
-      });
-
-      return res.status(200).send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
 
   app.delete("/api/users/evaluations/:id", requireLogin, async (req, res) => {
     try {
@@ -112,7 +114,7 @@ module.exports = app => {
     }
   });
 
-  app.get("/api/users/evaluations/:enterprise_id", async (req, res) => {
+  app.get("/api/users/evaluations/:enterprise_id", requireLogin,async (req, res) => {
     try {
       const user_id = req.user.id;
       const { enterprise_id } = req.params;
@@ -130,20 +132,32 @@ module.exports = app => {
     }
   });
 
-  app.put("/api/users/settings", async (req, res) => {
+  app.patch("/api/users", requireLogin, async (req, res) => {
+    try {
+      const id = req.user.id;
+      const { attributes } = req.body;
+      await Users.update({ ...attributes }, { where: { id } });
+      const result = await Users.findOne({ where: { id } });
+      return res.status(200).send(result);
+    } catch (e) {
+      console.log(e)
+    }
+  })
+
+  app.put("/api/users/settings", requireLogin, async (req, res) => {
     try {
       const id = req.user.id;
       const { name, email, ...sectors } = req.body;
 
       await Users.update({ name, email }, { where: { id } });
-      await UsersSectors.destroy({ where: {user_id: id}});
 
-      const promises = _.map(sectors, (value, sector_id)=> {
-        if (value) {
-            return UsersSectors.create({user_id: id, sector_id})
-        }
-      });
-      await Promise.all(promises)
+        await UsersSectors.destroy({ where: {user_id: id}});
+        const promises = _.map(sectors, (value, sector_id)=> {
+          if (value) {
+              return UsersSectors.create({user_id: id, sector_id})
+          }
+        });
+        await Promise.all(promises)
      const result = await Users.findOne({
         where: { id },
         include: [{ model: UsersSectors }]
@@ -155,44 +169,9 @@ module.exports = app => {
     }
   });
 
-  app.put("/api/users/:id", async (req, res) => {
+  app.put("/api/users/:id", requireLogin, async (req, res) => {
     try {
       const { id } = req.params;
-      const { name } = req.body;
-      const result = await Users.update({ name }, { where: { id } });
-      return res.status(200).send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  /*
-  * Admins
-  */
-  app.get("/api/admins", async (req, res) => {
-    try {
-      const result = await Users.findAll();
-      return res
-        .status(200)
-        .set("x-Total-Count", result.length)
-        .send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  app.get("/api/admins/:id", async (req, res) => {
-    try {
-      const { id } = req.params;
-      const result = await Users.findOne({ where: { id } });
-      return res.status(200).send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  app.put("/api/admins/:id", async (req, res) => {
-    try {
       const { name } = req.body;
       const result = await Users.update({ name }, { where: { id } });
       return res.status(200).send(result);
