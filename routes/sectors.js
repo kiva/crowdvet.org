4864b834e6bf661454a3f59de9fd4350df1534bb
walter <buczacka.walter@gmail.com>
2018-08-03 14:28:44 -0300

    update sectors routes

--- routes/sectors.js
+++ routes/sectors.js
@@ -3,8 +3,7 @@ const requireLogin = require("../middlewares/requireLogin");
 module.exports = app => {
   const { Enterprises, Evaluations, Sectors } = app.datasource.models.Enterprises.model;
 
-
-  app.get("/api/sectors", async (req, res) => {
+  app.get("/api/sectors", requireLogin, async (req, res) => {
     try {
       const result = await Sectors.findAll();
       return res
@@ -16,35 +15,10 @@ module.exports = app => {
     }
   });
 
-  app.get("/api/sectors/:id", async (req, res) => {
+  app.get("/api/sectors/:id", requireLogin, async (req, res) => {
     try {
       const { id } = req.params;
       const result = await Sectors.findOne({ where: { id } });
-      return res.status(200).send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  app.post("/api/sectors", async (req, res) => {
-    try {
-      const { name } = req.body;
-      const result = await Sectors.create({
-        name
-      });
-      return res.status(200).send(result);
-    } catch (e) {
-      console.log(e);
-    }
-  });
-
-  app.put("/api/sectors/:id", async (req, res) => {
-    try {
-      const { name } = req.body;
-      const result = await Sectors.update(
-        { name },
-        { where: { id } }
-      );
       return res.status(200).send(result);
     } catch (e) {
       console.log(e);
