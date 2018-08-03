0a032fd535c1e6f087d6029edb04101753511c26
walter <buczacka.walter@gmail.com>
2018-08-03 14:20:44 -0300

    add validation

--- client/src/components/Admin/Evaluations.js
+++ client/src/components/Admin/Evaluations.js
@@ -26,6 +26,9 @@ import {
 
 import Radio from "./Radio";
 
+const required = (message = 'This field is Required') =>
+    value => value ? undefined : message;
+
 const EvaluationFilter = props => (
   <Filter {...props}>
     <TextInput label="Official Votes" source="type" defaultValue="official" />
@@ -56,22 +59,31 @@ export const EvaluationsCreate = props => {
   return (
     <Create title="Create" {...props}>
       <SimpleForm>
-        <ReferenceInput
+
+      <ReferenceInput validate={required()}
+        label="User"
+        source="user_id"
+        reference="users"
+      >
+        <SelectInput optionText="name"  />
+      </ReferenceInput>
+
+        <ReferenceInput validate={required()}
           label="Enterprise"
           source="enterprise_id"
           reference="enterprises"
         >
-          <SelectInput optionText="name" />
+          <SelectInput optionText="name"  />
         </ReferenceInput>
 
 
-        <RadioButtonGroupInput fullWidth source="impact" choices={[
+        <RadioButtonGroupInput validate={required()}  fullWidth source="impact" choices={[
             { id: '1', name: 'This indicates any social enterprise you feel has negative social impact, or takes advantage of people - either the people it claims to serve, or other parties.' },
             { id: '2', name: 'This company has no discernable social impact at all. Most for-profit companies fall into this category rating.' },
             { id: '3', name: 'This company has one or more of the following: - Questionable social impact; - Social impact based on donations; - Possible social impact that is not integral to the business model.' }
         ]} />
 
-        <RadioButtonGroupInput fullWidth source="model" choices={[
+        <RadioButtonGroupInput validate={required()} fullWidth source="model" choices={[
             { id: '1', name: 'This business is not making money. It is dependant on donations and grants.​' },
             { id: '2', name: 'This business has some income, but is mostly dependent on grants and donations, somewhere around a 20:80 ratio.' },
             { id: '3', name: 'This company has raised cash capital, but has minimal sales, or questionably low sales volume considering its current lifespan.' }
@@ -85,6 +97,14 @@ export const EvaluationsEdit = props => (
   <Edit title="Edit" {...props}>
     <SimpleForm>
       <DisabledInput source="id" />
+      <ReferenceInput validate={required()}
+        label="User"
+        source="user_id"
+        reference="users"
+      >
+        <SelectInput optionText="name"  />
+      </ReferenceInput>
+
       <ReferenceInput
         label="Enterprise"
         source="enterprise_id"
@@ -100,6 +120,12 @@ export const EvaluationsEdit = props => (
       ]} />
 
       <RadioButtonGroupInput fullWidth source="model" choices={[
+          { id: '1', name: 'This business is not making money. It is dependant on donations and grants.​' },
+          { id: '2', name: 'This business has some income, but is mostly dependent on grants and donations, somewhere around a 20:80 ratio.' },
+          { id: '3', name: 'This company has raised cash capital, but has minimal sales, or questionably low sales volume considering its current lifespan.' }
+      ]} />
+
+      <RadioButtonGroupInput fullWidth source="Prioritization" choices={[
           { id: '1', name: 'This business is not making money. It is dependant on donations and grants.​' },
           { id: '2', name: 'This business has some income, but is mostly dependent on grants and donations, somewhere around a 20:80 ratio.' },
           { id: '3', name: 'This company has raised cash capital, but has minimal sales, or questionably low sales volume considering its current lifespan.' }
