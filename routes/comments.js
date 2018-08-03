b0d2caff51b58f51c0c66975cc5f5e42e76ccbd7
walter <buczacka.walter@gmail.com>
2018-08-03 14:31:45 -0300

    update comments routes

---
+++ routes/comments.js
@@ -0,0 +1,92 @@
+const requireLogin = require("../middlewares/requireLogin");
+
+module.exports = app => {
+  const {
+    Enterprises,
+    Evaluations,
+    Sectors,
+    Comments,
+    CommentVotes
+  } = app.datasource.models.Enterprises.model;
+
+  app.get("/api/enterprises/:id/comments", requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await Comments.findAll({
+        where: { enterprise_id: id, comment_id: null },
+        include: [{ model: Comments, as: "Replies" }, { model: CommentVotes }]
+      });
+
+      return res.status(200).send(result);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  app.post("/api/enterprises/:id/comments", requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user_id = req.user.id;
+      const { text } = req.body;
+
+      const comment = await Comments.create({
+        user_id,
+        enterprise_id: id,
+        text
+      });
+
+      return res.status(200).send(comment);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  app.post("/api/comments/:id/replies", requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user_id = req.user.id;
+      const { text, enterprise_id } = req.body;
+
+      const comment = await Comments.create({
+        user_id,
+        text,
+        comment_id: id,
+        enterprise_id
+      });
+
+      return res.status(200).send(comment);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  app.post("/api/comments/:id/votes", requireLogin, async (req, res) => {
+    try {
+      const { id } = req.params;
+      const user_id = req.user.id;
+
+      const vote = await CommentVotes.findOne({
+        where: { user_id, comment_id: id }
+      });
+
+      if (vote) {
+        CommentVotes.destroy({ where: { user_id, comment_id: id } });
+        const comment = await Comments.findOne({
+          where: { id },
+          include: [{ model: Comments, as: "Replies" }, { model: CommentVotes }]
+        });
+        return res.status(200).send(comment);
+      }
+      const newVote = await CommentVotes.create({ user_id, comment_id: id });
+      const comment = await Comments.findOne({
+        where: { id },
+        include: [{ model: Comments, as: "Replies" }, { model: CommentVotes }]
+      });
+
+      return res.status(200).send(comment);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+};
