04775e8030adc62308acd90730e86cdc4d58a079
walter <buczacka.walter@gmail.com>
2018-08-03 17:29:44 -0300

    fix route path

--- client/src/components/Admin/actions/index.js
+++ client/src/components/Admin/actions/index.js
@@ -3,7 +3,7 @@ import { showNotification as showNotificationAction } from 'react-admin';
 import { push } from 'react-router-redux';
 
 export const importEnterprises = (ids) => {
-  axios.post('/api/enterprises', { ids })
+  axios.post('/api/admin/enterprises', { ids })
   .then(res => {
     showNotificationAction('Applications imported');
     push('/applications');
