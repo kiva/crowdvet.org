a36f3435007c8467057cde4021be63139de3ee6d
walter <buczacka.walter@gmail.com>
2018-08-04 12:01:27 -0300

    show comments

--- client/src/components/EvaluationResults.js
+++ client/src/components/EvaluationResults.js
@@ -8,8 +8,8 @@ import KivaMessage from "./KivaMessage";
 import _ from "lodash";
 import moment from "moment";
 import Countdown from "react-countdown-now";
-import utils from './utils'
-import idgen from './idgen';
+import utils from "./utils";
+import idgen from "./idgen";
 
 class EvaluationResults extends Component {
   constructor(props) {
@@ -19,7 +19,7 @@ class EvaluationResults extends Component {
       menu: {
         1: { text: "Review", url: `/application/${id}` },
         2: { text: "Evaluation", url: `/users/evaluations/${id}` },
-        3: { text: "Results", url:"" }
+        3: { text: "Results", url: "" }
       },
       active: 3
     };
@@ -37,14 +37,24 @@ class EvaluationResults extends Component {
   };
 
   render() {
-    const { auth, questions, enterprise, evaluation, officialEvaluation } = this.props;
+    const {
+      auth,
+      questions,
+      enterprise,
+      evaluation,
+      officialEvaluation
+    } = this.props;
     if (!auth) return null;
     if (!enterprise) return null;
     if (!evaluation) return null;
 
     this.sector = enterprise.Sector ? enterprise.Sector.name : "Water";
     const imgName = `/sectors/${this.sector}.jpg`;
-    const message = utils.getMessage(enterprise, evaluation, officialEvaluation);
+    const message = utils.getMessage(
+      enterprise,
+      evaluation,
+      officialEvaluation
+    );
     return (
       <div>
         <TopMenu onSubMenuChange={this.onSubMenuChange} />
@@ -71,14 +81,18 @@ class EvaluationResults extends Component {
           <div className="row flow-text center">
             <h3 className="col s12">Evaluation Results</h3>
           </div>
-          <KivaMessage message={message.message}/>
+          <KivaMessage message={message.message} />
           <div className="row">{this.renderEvaluation()}</div>
           <div className="row">
             <div className="col s6">
-              <Link to={"/user"} className="btn button-large btn-results">Previous Page</Link>
+              <Link to={"/user"} className="btn button-large btn-results">
+                Previous Page
+              </Link>
             </div>
             <div className="col s6">
-              <Link to={message.page} className="btn button-large btn-results">{message.text}</Link>
+              <Link to={message.page} className="btn button-large btn-results">
+                {message.text}
+              </Link>
             </div>
           </div>
         </div>
@@ -102,27 +116,31 @@ class EvaluationResults extends Component {
 
   renderRadios(name, choices, question) {
     const content = _.map(choices, choice => {
-      const id=idgen();
+      const id = idgen();
       return (
-          <div key={idgen()} className="col s12 m2 center">
-            <label className="radio-evaluation">
-              <input
-                id={id}
-                name={name}
-                type="radio"
-                checked={this.props.evaluation[name] == choice.score ? true : false}
-                disabled={true}
-              />
-              <label htmlFor={id} id="radio-text">{choice.score}</label>
+        <div key={idgen()} className="col s12 m2 center">
+          <label className="radio-evaluation">
+            <input
+              id={id}
+              name={name}
+              type="radio"
+              checked={
+                this.props.evaluation[name] == choice.score ? true : false
+              }
+              disabled={true}
+            />
+            <label htmlFor={id} id="radio-text">
+              {choice.score}
             </label>
-          </div>
+          </label>
+        </div>
       );
     });
 
     return (
       <div>
         <div>
-            <div className="question">{question.text}</div>
+          <div className="question">{question.text}</div>
         </div>
         {content}
       </div>
@@ -131,21 +149,53 @@ class EvaluationResults extends Component {
 
   renderEvaluation() {
     const { evaluation } = this.props;
-    let text = ""
+    console.log(evaluation);
+    let text = "";
     return (
       <div>
-        <div>
-          <div  className="row">
-          {this.renderRadios("impact", impactChoices, impactQuestion)}
+          <div className="row">
+            {this.renderRadios("impact", impactChoices, impactQuestion)}
           </div>
-          <div  className="row">
-          {this.renderRadios("model", modelChoices, modelQuestion)}
+          <div className="row">
+            <div className="col s12">
+              <p>{evaluation.impact}: {_.mapKeys(impactChoices, "score")[evaluation.impact].text}</p>
+            </div>
+            <div className="col s12">
+              <p><input disabled={true} type="text" value={evaluation.impactComment} /></p>
+            </div>
           </div>
-          <div  className="row">
-          {this.renderRadios("prioritization", prioritizationChoices, prioritizationQuestion)}
+          <div className="row">
+            {this.renderRadios("model", modelChoices, modelQuestion)}
           </div>
+          <div className="row">
+            <div className="col s12">
+              <p>{evaluation.model}: {_.mapKeys(modelChoices, "score")[evaluation.model].text}</p>
+            </div>
+            <div className="col s12">
+              <p><input disabled={true} type="text" value={evaluation.modelComment} /></p>
+            </div>
+          </div>
+          <div className="row">
+            {this.renderRadios(
+              "prioritization",
+              prioritizationChoices,
+              prioritizationQuestion
+            )}
+          </div>
+          <div className="row">
+            <div className="col s12">
+            <p>{evaluation.prioritization}: {_.mapKeys(prioritizationChoices, "score")[evaluation.prioritization].text}</p>
+            </div>
+            <div className="col s12">
+              <p><input disabled={true} type="text" value={evaluation.prioritizationComment} /></p>
+            </div>
+          </div>
+        <div className="row">
+            <div className="question">4. What else should Kiva know about this enterprise?</div>
+            <div className="col s12 answer-result">
+              <p><input disabled={true} type="text" value={evaluation.comment} /></p>
+            </div>
         </div>
-        <div className="col s12 answer-result"><p>{text}</p></div>
       </div>
     );
   }
@@ -164,13 +214,18 @@ function mapStateToProps(
   };
 }
 
-const impactQuestion =
-  {text: "1. Overall, the enterprise has a meaningful impact on low income or excluded communities [strongly disagree - strongly agree] *"}
-const modelQuestion =
-  {text: "2. Overall, the enterprise has a viable business model [strongly disagree - strongly agree] *"}
-  const prioritizationQuestion =
-    {text: "3. Overall, Kiva should move forward with this application and submit this loan for crowdfunding [strongly disagree - strongly agree] *"}
-
+const impactQuestion = {
+  text:
+    "1. Overall, the enterprise has a meaningful impact on low income or excluded communities [strongly disagree - strongly agree] *"
+};
+const modelQuestion = {
+  text:
+    "2. Overall, the enterprise has a viable business model [strongly disagree - strongly agree] *"
+};
+const prioritizationQuestion = {
+  text:
+    "3. Overall, Kiva should move forward with this application and submit this loan for crowdfunding [strongly disagree - strongly agree] *"
+};
 
 const impactChoices = [
   {
@@ -235,15 +290,13 @@ const modelChoices = [
     score: 6,
     text:
       "This company is already healthily profitable and sustainable, and has the ability to scale.​"
-  },
-
+  }
 ];
 
 const prioritizationChoices = [
   {
     score: 1,
-    text:
-      "I really wouldn’t recommend moving forward with this enterprise."
+    text: "I really wouldn’t recommend moving forward with this enterprise."
   },
   {
     score: 2,
@@ -252,13 +305,11 @@ const prioritizationChoices = [
   },
   {
     score: 3,
-    text:
-      "I’m not sold on this. This isn’t a clear ‘yes’ for Kiva."
+    text: "I’m not sold on this. This isn’t a clear ‘yes’ for Kiva."
   },
   {
     score: 4,
-    text:
-      "This sounds suitable for Kiva. I would recommend considering this."
+    text: "This sounds suitable for Kiva. I would recommend considering this."
   },
   {
     score: 5,
