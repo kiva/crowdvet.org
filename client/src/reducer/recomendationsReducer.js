716c3ae514e714934aa10d0f9257a74c82134336
walter <buczacka.walter@gmail.com>
2018-08-23 12:09:14 -0300

    remove map keys

--- client/src/reducer/recomendationsReducer.js
+++ client/src/reducer/recomendationsReducer.js
@@ -1,10 +1,10 @@
 import { FETCH_RECOMENDATIONS } from '../actions/types';
-import _ from "lodash";
+
 
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_RECOMENDATIONS:
-      return _.mapKeys(action.payload, "id");
+      return { ...action.payload }
     default:
       return state;
   }
