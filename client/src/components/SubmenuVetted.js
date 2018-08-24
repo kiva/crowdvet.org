5a858de31a5b311ca27f120e04b79e7397958c5a
walter <buczacka.walter@gmail.com>
2018-08-24 15:54:00 -0300

    update submenu vetted

--- client/src/components/SubmenuVetted.js
+++ client/src/components/SubmenuVetted.js
@@ -1,46 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import * as actions from '../actions';
-import _ from 'lodash';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import * as actions from "../actions";
+import _ from "lodash";
 
 class SubMenuVetted extends Component {
-
   onHandleClickVetted() {
-    this.props.onSubMenuChange(
-      {
-        1: 'Vetted Enterprises',
-        2: 'Vetting History'
-      },
-      1
-    )
+    this.props.onSubMenuChange({
+      vetted: { active: true },
+      history: { active: false }
+    });
   }
 
   onHandleClickHistory() {
-    this.props.onSubMenuChange(
-      {
-        1: 'Vetted Enterprises',
-        2: 'Vetting History'
-      },
-      2
-    )
+    this.props.onSubMenuChange({
+      vetted: { active: false },
+      history: { active: true }
+    });
   }
 
   render() {
-      const menu = (
-        <div>
-        <div className='col s6 center'>
-        <button autoFocus onClick={this.onHandleClickVetted.bind(this)} className="btn btn-flat btn-large dashboard-item">
-          Vetted Enterprises
-        </button>
+    const activeClass = "SubMenuActive";
+    const inactiveClass = "SubMenuInactive";
+    const menu = (
+      <div>
+        <div className={`col s6 center ${this.props.menu.vetted.active && activeClass || inactiveClass}`}>
+          <a
+            onClick={this.onHandleClickVetted.bind(this)}
+            className={`btn btn-flat btn-large dashboard-item`}
+          >
+            Vetted Enterprises
+          </a>
         </div>
-        <div className="col s6 center">
-        <button onClick={this.onHandleClickHistory.bind(this)} className="btn btn-flat btn-large dashboard-item">
-          Vetting History
-        </button>
+        <div className={`col s6 center ${this.props.menu.history.active && activeClass || inactiveClass}`}>
+          <a
+            onClick={this.onHandleClickHistory.bind(this)}
+            className={`btn btn-flat btn-large dashboard-item
+            `}
+          >
+            Vetting History
+          </a>
         </div>
-        </div>
-      );
+      </div>
+    );
 
     return menu;
   }
