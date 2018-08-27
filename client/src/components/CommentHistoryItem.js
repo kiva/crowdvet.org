d4cc716c30e56d37466b69779e97f04a50b018ce
walter <buczacka.walter@gmail.com>
2018-08-27 11:59:20 -0300

    add info to comment history and load more

--- client/src/components/CommentHistoryItem.js
+++ client/src/components/CommentHistoryItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./CommentHistory.css";
+import moment from 'moment';
 
 export default ({ comment, enterprise }) => {
   return (
     <div className="col s12 m8 offset-m2 gray-background">
       <p>{comment.comment_id ? getReplied(enterprise) : getCommented(enterprise)}</p>
       <p>{comment.text}</p>
+      <p className="grey-text">{moment(comment.created_at).format("MMMM DD, YYYY")}</p>
     </div>
   );
 };
--- client/src/components/CommentItem.js
+++ client/src/components/CommentItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import ReplyForm from "./ReplyForm";
 import ReplyItem from "./ReplyItem";
 import _ from "lodash";
@@ -7,11 +7,13 @@ import heart from "./heart.svg";
 import heartGrey from "./heart-grey.svg";
 import reply from "./reply.svg";
 import * as actions from "../actions";
-
+import moment from "moment";
 
 function renderReplies(replies) {
   return _.map(replies, reply => {
-    return <ReplyItem reply={reply} />;
+    return (
+      <ReplyItem reply={reply} user={reply.User} date={reply.created_at} />
+    );
   });
 }
 
@@ -28,8 +30,7 @@ class CommentItem extends Component {
   onHandleVote() {
     const { id } = this.props.comment;
     const vote = !this.state.vote;
-    this.props.VoteComment({ id, vote})
-
+    this.props.VoteComment({ id, vote });
   }
   render() {
     const { comment, user } = this.props;
@@ -42,20 +43,56 @@ class CommentItem extends Component {
     return (
       <li>
         <div className="row collapsible-header grey-background comment-height">
-          <div className="col s1 comment-votes center">{comment.CommentVotes ? comment.CommentVotes.length : 0} Votes</div>
+          <div className="col s1 comment-votes center">
+            <div style={{ fontSize: "20px" }}>
+              {comment.CommentVotes ? comment.CommentVotes.length : 0}
+            </div>
+            <span>Votes</span>
+          </div>
           <div className="col s11">{comment.text}</div>
         </div>
         <div className="collapsible-body">
-          <div className="row grey-background" id="comment-icons">
-            <div onClick={this.onHandleVote.bind(this)} className="col s12 m1">
-              <img src={heartImg} />
+          <div className="row grey-background">
+            <div className="col s2">
+              <p>
+                <img
+                  className="circle responsive-img"
+                  src={user.image}
+                  style={{ maxWidth: "50px" }}
+                />
+              </p>
+              <p className="comment-height">{user.name}</p>
             </div>
-            <div onClick={this.onHandleClick.bind(this)} className="col s12 m1">
-              <img src={reply} />
+            <div className="col s8 grey-background">
+              <p className="comment-height">{comment.text}</p>
+            </div>
+            <div className="col s2 grey-background">
+              <p className="comment-height">
+                {moment(comment.created_at).format("MMMM DD, YYYY")}
+              </p>
+            </div>
+            <div className="col s12 grey-background" id="comment-icons">
+              <div
+                onClick={this.onHandleVote.bind(this)}
+                className="col s12 m1"
+              >
+                <img src={heartImg} />
+              </div>
+              <div
+                onClick={this.onHandleClick.bind(this)}
+                className="col s12 m1"
+              >
+                <img src={reply} />
+              </div>
             </div>
           </div>
+
           <div>
-            <ReplyForm hide={hide} id={comment.id} enterprise_id={comment.enterprise_id} />
+            <ReplyForm
+              hide={hide}
+              id={comment.id}
+              enterprise_id={comment.enterprise_id}
+            />
           </div>
           <ul>{comment.Replies ? renderReplies(comment.Replies) : ""}</ul>
         </div>
