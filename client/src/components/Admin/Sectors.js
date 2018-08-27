e4950fab8acb05123b9141c14cc191b829f29781
walter <buczacka.walter@gmail.com>
2018-08-27 12:24:51 -0300

    add validation to sectors

--- client/src/components/Admin/Sectors.js
+++ client/src/components/Admin/Sectors.js
@@ -15,6 +15,21 @@ import {
   ReferenceInput
 } from 'react-admin';
 
+
+const required = (message = 'Required') =>
+    value => value ? undefined : message;
+
+
+const validateUserCreation = (values) => {
+    const errors = {};
+    if (!values.image && !values.link) {
+        errors.image = ['Must enter image name or image link.'];
+        errors.link = ['Must enter image name or image link.'];
+    }
+
+    return errors
+};
+
 export const SectorsList = props => (
   <List {...props}>
     <Datagrid>
@@ -27,17 +42,20 @@ export const SectorsList = props => (
 
 export const SectorsCreate = props => (
   <Create title="Create" {...props}>
-    <SimpleForm>
-    <TextInput source="name" />
+    <SimpleForm validate={validateUserCreation}>
+    <TextInput source="name" validate={ required() } />
+    <LongTextInput label="Image Name" source="image" />
+    <LongTextInput label="Image Link" source="link" />
     </SimpleForm>
   </Create>
 );
 
 export const SectorsEdit = props => (
   <Edit title="Edit" {...props}>
-    <SimpleForm>
-      <DisabledInput source="id" />
-      <TextInput source="name" />
+    <SimpleForm validate={validateUserCreation}>
+      <TextInput source="name" validate={ required() } />
+      <LongTextInput label="Image Name" source="image" />
+      <LongTextInput label="Image Link" source="link" />
     </SimpleForm>
   </Edit>
 );
