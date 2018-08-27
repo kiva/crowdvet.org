ea322e4266f418a727892759cc0e6cfec1f2b8fc
walter <buczacka.walter@gmail.com>
2018-08-27 11:59:34 -0300

    add info to replies

--- client/src/components/ReplyItem.js
+++ client/src/components/ReplyItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
-function ReplyItem({reply}) {
+function ReplyItem({reply, date, user}) {
   return(
-    <div className="row">
-    <li className="col s11 offset-s1 grey-background comment-height">
-      <span className="comment-height">{reply.text}</span>
+    <div className="row grey-background">
+    <li className="col s2 grey-background">
+      <p><img className="circle responsive-img" src={user.image} style={{maxWidth:"50px"}}/></p>
+      <p className="comment-height">{user.name}</p>
+    </li>
+    <li className="col s8 grey-background">
+      <p className="comment-height">{reply.text}</p>
+    </li>
+    <li className="col s2 grey-background">
+      <p className="comment-height">{moment(date).format("MMMM DD, YYYY")}</p>
     </li>
     </div>
   )
