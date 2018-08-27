85a5c77c761cd752bba0f524eb3da886abb1a1a9
walter <buczacka.walter@gmail.com>
2018-08-27 11:58:48 -0300

    add info to comments

--- client/src/components/CommentHistory.js
+++ client/src/components/CommentHistory.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
 import CommentItem from "./CommentHistoryItem";
 import _ from "lodash";
 
-export default props => {
-  return (
-    <div className="row">
-      <div className="col s12">
-        <h4 className="center">Your Comment History</h4>
+class CommentHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: 3, limit: 3 };
+    this.onHandleClick = this.onHandleClick.bind(this);
+  }
+
+  render() {
+    let hide = "";
+    if (this.props.comments.length == 0) hide = "hide";
+    if (Object.keys(this.props.comments).length <= this.state.show) {
+      hide = "hide";
+    }
+
+    const comments = _.slice(_.values(this.props.comments),0, this.state.show);
+
+    return (
+      <div className="row">
+        <div className="col s12">
+          <h4 className="center">Your Comment History</h4>
+        </div>
+        {renderComments(comments, this.props.enterprises)}
+        <div className={`col s12 m8 offset-m2 gray-background center ${hide}`}><a onClick={this.onHandleClick}className="btn-flat">Load More</a></div>
+        <div className="col s12">
+          <h4 className="center">Certificates History</h4>
+        </div>
       </div>
-      {renderComments(props.comments, props.enterprises)}
-      <div className="col s12">
-        <h4 className="center">Certificates History</h4>
-      </div>
-    </div>
-  );
+    );
+  }
+
+  onHandleClick(e) {
+    e.preventDefault();
+    const show = this.state.show + this.state.limit;
+    this.setState({ show });
+  }
 };
 
 function renderComments(comments, enterprises) {
@@ -29,3 +52,5 @@ function renderComments(comments, enterprises) {
     );
   });
 }
+
+export default CommentHistory;
