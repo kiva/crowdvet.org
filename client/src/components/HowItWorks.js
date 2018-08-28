7daba7f4db54a48ddacd0d4078ea7c437b90f99b
walter <buczacka.walter@gmail.com>
2018-08-28 13:48:38 -0300

    separate jsx into a new component

---
+++ client/src/components/HowItWorks.js
@@ -0,0 +1,50 @@
+import React from "react";
+import arrow from "./Arrow.svg";
+import howWorks from "./HowWorks.svg";
+import { Link } from "react-router-dom";
+
+const HowItWorks = props => {
+  const startVetting = props.auth ? <Link className="btn-flat start-vetting-btn"
+    to={"/user"}>Start Vetting</Link> : <Link className="btn-flat start-vetting-btn modal-trigger" to={"#modal1"}>Start Vetting</Link>
+  return (
+    <div className="row">
+      <div className="row" />
+      <div className="col s12 center font-34 bold">
+        We want your individual, unique view of the world!
+      </div>
+      <p className="col s12 center font-26">
+        A diverse crowd helps Kiva prioritize borrowers for internal due
+        diligence processes. DSE loan applications are posted as they are
+        received and will accept evaluations for up to 60 days.
+      </p>
+      <p className="center">
+        <img src={howWorks} />
+      </p>
+      <div className="col s12 center font-34 bold">Set your own Schedule</div>
+      <div className="col s12 center font-26 green-text">Step 1</div>
+      <div className="col s12 center font-26">Join at any time</div>
+      <p className="center">
+        <img src={arrow} />
+      </p>
+      <div className="col s12 center font-26 green-text">Step 2</div>
+      <div className="col s12 center font-26">
+        Vet any number of enterprises at your own pace
+      </div>
+      <p className="center">
+        <img src={arrow} />
+      </p>
+      <div className="col s12 center font-26 green-text">Step 3 </div>
+      <div className="col s12 center font-26">
+        Receive feedback, scores, and badges
+      </div>
+      <p className="col s6 center">{startVetting}</p>
+      <p className="col s6 center">
+        <Link className="btn-flat learn-more-btn" to={"/further-reading"}>
+          Learn More
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default HowItWorks;
