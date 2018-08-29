0b8f74e24c2ca94953e636b3c620754735bfeb70
walter <buczacka.walter@gmail.com>
2018-08-29 16:51:35 -0300

    update content

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -7,42 +7,54 @@ const HowItWorks = props => {
   const startVetting = props.auth ? <Link className="btn-flat start-vetting-btn"
     to={"/user"}>Start Vetting</Link> : <Link className="btn-flat start-vetting-btn modal-trigger" to={"#modal1"}>Start Vetting</Link>
   return (
-    <div className="row">
-      <div className="row" />
-      <div className="col s12 center font-34 bold">
-        We want your individual, unique view of the world!
+    <div>
+      <div className="row">
+        <div className="row" />
+        <div className="col s12 center font-34 bold">
+          We want your individual, unique view of the world!
+        </div>
+        <p className="col s12 center font-26">
+          A diverse crowd helps Kiva prioritize borrowers for internal due
+          diligence processes. DSE loan applications are posted as they are
+          received and will accept evaluations for up to 60 days.
+        </p>
+        <p className="center">
+          <img src={howWorks} />
+        </p>
+        <div className="col s12 center font-34 bold">
+          Set your own Schedule
+        </div>
+        <div className="col s12 center font-26 green-text">Step 1</div>
+        <div className="col s12 center font-26">Join at any time</div>
+        <p className="center">
+          <img src={arrow} />
+        </p>
+        <div className="col s12 center font-26 green-text">Step 2</div>
+        <div className="col s12 center font-26">
+          Vet any number of enterprises at your own pace
+        </div>
+        <p className="center">
+          <img src={arrow} />
+        </p>
+        <div className="col s12 center font-26 green-text">Step 3 </div>
+        <div className="col s12 center font-26">
+          Receive feedback, scores, and badges
+        </div>
       </div>
-      <p className="col s12 center font-26">
-        A diverse crowd helps Kiva prioritize borrowers for internal due
-        diligence processes. DSE loan applications are posted as they are
-        received and will accept evaluations for up to 60 days.
-      </p>
-      <p className="center">
-        <img src={howWorks} />
-      </p>
-      <div className="col s12 center font-34 bold">Set your own Schedule</div>
-      <div className="col s12 center font-26 green-text">Step 1</div>
-      <div className="col s12 center font-26">Join at any time</div>
-      <p className="center">
-        <img src={arrow} />
-      </p>
-      <div className="col s12 center font-26 green-text">Step 2</div>
-      <div className="col s12 center font-26">
-        Vet any number of enterprises at your own pace
+      <div className="row">
+        <div className="col s12 center font-26 green-text">Result:</div>
+        <div className="col s12 center font-26">
+          Based on your input, Kiva will reject or approve the loan
+        </div>
       </div>
-      <p className="center">
-        <img src={arrow} />
-      </p>
-      <div className="col s12 center font-26 green-text">Step 3 </div>
-      <div className="col s12 center font-26">
-        Receive feedback, scores, and badges
+      <div className="row" style={{ marginTop: "50px" }}>
+        <p className="col s6 center">{startVetting}</p>
+        <p className="col s6 center">
+          <Link className="btn-flat learn-more-btn" to={"/further-reading"}>
+            Learn More
+          </Link>
+        </p>
       </div>
-      <p className="col s6 center">{startVetting}</p>
-      <p className="col s6 center">
-        <Link className="btn-flat learn-more-btn" to={"/further-reading"}>
-          Learn More
-        </Link>
-      </p>
     </div>
   );
 };
--- client/src/components/HowWorks.js
+++ client/src/components/HowWorks.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Carousel from "./Carousel-mz";
-import carousel from './caroussel-1.png';
+import carousel from "./caroussel-1.png";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import idgen from "./idgen";
@@ -11,56 +11,98 @@ import SignUpModal from "./SignUpModal";
 import SubMenu from "./SubMenuLearn";
 
 class HowWorks extends Component {
-
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
   render() {
-    const header = <div className="white-text flow-text center-element learn-about">How can you help social enterprises make the world
-    a better place, even if you can’t make a loan?</div>
+    const header = (
+      <div className="white-text flow-text center-element learn-about">
+        How can you help social enterprises make the world a better place, even
+        if you can’t make a loan?
+      </div>
+    );
     return (
       <div>
-      <SubMenu menu={{
-        about: { active: false },
-        howWorks: { active: true },
-        further: { active: false }
-      }} />
-      <Carousel images={[carousel, carousel]}
-      options={{ fullWidth: true, indicators: true }} carouselId={idgen().toString()}
-       fixedText={header}
-      />
-      <SignUpModal />
-      <div className="container">
-        {this.renderContent()}
+        <SubMenu
+          menu={{
+            about: { active: false },
+            howWorks: { active: true },
+            further: { active: false }
+          }}
+        />
+        <Carousel
+          images={[carousel, carousel]}
+          options={{ fullWidth: true, indicators: true }}
+          carouselId={idgen().toString()}
+          fixedText={header}
+        />
+        <SignUpModal />
+        <div className="container">{this.renderContent()}</div>
       </div>
-      </div>
-    )
+    );
   }
 
   renderContent() {
-    const startVetting = this.props.auth ? <Link className="btn-flat start-vetting-btn" to={"/user"}>Start Vetting</Link> : <Link className="btn-flat start-vetting-btn modal-trigger" to={"#modal1"}>Start Vetting</Link>
+    const startVetting = this.props.auth ? (
+      <Link className="btn-flat start-vetting-btn" to={"/user"}>
+        Start Vetting
+      </Link>
+    ) : (
+      <Link className="btn-flat start-vetting-btn modal-trigger" to={"#modal1"}>
+        Start Vetting
+      </Link>
+    );
     return (
-      <div className="row">
-        <div className="row" />
-        <div className="col s12 center font-34 bold">We want your individual, unique view of the world!</div>
-        <p className="col s12 center font-26">A diverse crowd helps Kiva prioritize borrowers for internal due diligence processes.
-          DSE loan applications are posted as they are received and will accept evaluations for up to 60 days.</p>
-        <p className="center"><img src={howWorks} /></p>
-          <div className="col s12 center font-34 bold">Set your own Schedule</div>
+      <div>
+        <div className="row">
+          <div className="row" />
+          <div className="col s12 center font-34 bold">
+            We want your individual, unique view of the world!
+          </div>
+          <p className="col s12 center font-26">
+            A diverse crowd helps Kiva prioritize borrowers for internal due
+            diligence processes. DSE loan applications are posted as they are
+            received and will accept evaluations for up to 60 days.
+          </p>
+          <p className="center">
+            <img src={howWorks} />
+          </p>
+          <div className="col s12 center font-34 bold">
+            Set your own Schedule
+          </div>
           <div className="col s12 center font-26 green-text">Step 1</div>
           <div className="col s12 center font-26">Join at any time</div>
-          <p className="center"><img src={arrow} /></p>
+          <p className="center">
+            <img src={arrow} />
+          </p>
           <div className="col s12 center font-26 green-text">Step 2</div>
-          <div className="col s12 center font-26">Vet any number of enterprises at your own pace</div>
-          <p className="center"><img src={arrow} /></p>
+          <div className="col s12 center font-26">
+            Vet any number of enterprises at your own pace
+          </div>
+          <p className="center">
+            <img src={arrow} />
+          </p>
           <div className="col s12 center font-26 green-text">Step 3 </div>
-          <div className="col s12 center font-26">Receive feedback, scores, and badges</div>
+          <div className="col s12 center font-26">
+            Receive feedback, scores, and badges
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 center font-26 green-text">Result:</div>
+          <div className="col s12 center font-26">
+            Based on your input, Kiva will reject or approve the loan
+          </div>
+        </div>
+        <div className="row" style={{ marginTop: "50px" }}>
+          <p className="col s6 center">{startVetting}</p>
           <p className="col s6 center">
-            {startVetting}
+            <Link className="btn-flat learn-more-btn" to={"/further-reading"}>
+              Learn More
+            </Link>
           </p>
-          <p className="col s6 center">
-            <Link className="btn-flat learn-more-btn" to={"/further-reading"} >Learn More</Link>
-          </p>
+        </div>
       </div>
-
-    )
+    );
   }
 }
 
