12dbbcd91ea4cdab8d13a63cc1114f896c6da10d
walter <buczacka.walter@gmail.com>
2018-08-29 16:51:50 -0300

    add description

--- client/src/components/KivaMessage.js
+++ client/src/components/KivaMessage.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
-import "./KivaMessage.css"
+import "./KivaMessage.css";
 
 function KivaMessage(props) {
-  const backgroundClass = props.background ? props.background : "background-regular";
+  const backgroundClass = props.background
+    ? props.background
+    : "background-regular";
 
   return (
     <div className="row">
-      <div className="col s12" style={{height:"100px"}}>
-        <div className={`btn kiva-message-btn ${backgroundClass}`}>
-          <div className="kiva-message-btn-text">{props.message}</div>
+      <div className="row">
+        <div className="col s12" style={{ height: "100px" }}>
+          <div className={`btn kiva-message-btn ${backgroundClass}`}>
+            <div className="kiva-message-btn-text">{props.message}</div>
+          </div>
         </div>
       </div>
+      <div className="col s12 center"><p className="green-text">{props.description}</p></div>
     </div>
   );
 }
