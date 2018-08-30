b934f347fbe512160abd9a94f0855621b9dbd249
walter <buczacka.walter@gmail.com>
2018-08-30 17:40:13 -0300

    add faqs

--- client/src/components/SubMenuLearn.js
+++ client/src/components/SubMenuLearn.js
@@ -7,9 +7,9 @@ const SubMenu = (props) => {
   const inactiveClass = "inactive-text";
   return (
     <div className="row center-align dashboard" style={{height:"50px"}}>
-        <div  className="col s4"><Link className={`btn-flat btn-submenu line-50 ${props.menu.about.active && activeClass || inactiveClass}`} to={"/learn"}>About Us</Link></div>
-        <div  className="col s4"><Link className={`btn-flat btn-submenu line-50 ${props.menu.howWorks.active && activeClass || inactiveClass}`} to={"/how-works"}>How CrowdVet Works</Link></div>
-        <div  className="col s4"><Link className={`btn-flat btn-submenu line-50 ${props.menu.further.active && activeClass || inactiveClass}`} to={"further-reading"}>Further Reading</Link></div>
+        <div  className="col s4 "><Link className={`btn-flat btn-submenu top-menu line-50 ${props.menu.about.active && activeClass || inactiveClass}`} to={"/learn"}>About Us</Link></div>
+        <div  className="col s4"><Link className={`btn-flat btn-submenu top-menu line-50 ${props.menu.howWorks.active && activeClass || inactiveClass}`} to={"/how-works"}>How CrowdVet Works</Link></div>
+        <div  className="col s4"><Link className={`btn-flat btn-submenu top-menu line-50 ${props.menu.further.active && activeClass || inactiveClass}`} to={"further-reading"}>Further Reading</Link></div>
     </div>
   )
 
