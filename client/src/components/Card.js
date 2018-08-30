cb8cd6fb47b6853b4f4f6094bce19928b3b81027
walter <buczacka.walter@gmail.com>
2018-08-30 17:36:10 -0300

    update styles

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, description, mainText }) => {
   return (
     <div className="col s12 m4 l4 card-review">
-      <div className="card-panel teal">
+      <div className="card-panel teal min-height-card">
         <div className="title">{name}</div>
         <span>
           <h3>{mainText}</h3>
