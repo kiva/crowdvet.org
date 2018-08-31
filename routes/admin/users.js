4530064d29b198cb6dfc324a6ae67924de6cc7e6
walter <buczacka.walter@gmail.com>
2018-08-31 14:04:38 -0300

    update users routes

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const _ = require("lodash");
+const HttpStatus = require('http-status');
 
 module.exports = app => {
 
@@ -26,21 +27,31 @@ module.exports = app => {
     }
   });
 
+  app.delete("/api/admin/users/:id", passport.authenticate("jwt"), async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Users.destroy({ where: { id } });
+      res.status(200).send({data: true});
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
   app.get("/api/admin/users/:id", passport.authenticate("jwt"), async (req, res) => {
     try {
       const { id } = req.params;
       const result = await Users.findOne({ where: { id } });
-      return res.status(200).send(result);
+      res.status(200).send(result)
+
     } catch (e) {
-      console.log(e);
+      res.status(HttpStatus.BAD_REQUEST).send({data: e});
     }
   });
 
   app.put("/api/admin/users/:id", passport.authenticate("jwt"), async (req, res) => {
     try {
       const { id } = req.params;
-      const { name } = req.body;
-      const result = await Users.update({ name }, { where: { id } });
+      const result = await Users.update({ ...req.body }, { where: { id } });
       return res.status(200).send(result);
     } catch (e) {
       console.log(e);
@@ -74,11 +85,30 @@ module.exports = app => {
 
   app.put("/api/admin/admins/:id", passport.authenticate("jwt"), async (req, res) => {
     try {
-      const { name } = req.body;
-      const result = await Users.update({ name }, { where: { id } });
+      const { id } = req.params;
+      const result = await Users.update({ ...req.body, password: Users.hash(req.body.password) }, { where: { id } });
       return res.status(200).send(result);
     } catch (e) {
       console.log(e);
     }
   });
+
+  app.post("/api/admin/admins", passport.authenticate("jwt"), async (req, res) => {
+    try {
+      const result = await Users.create({ ...req.body, admin: true });
+      return res.status(200).send(result);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+    app.delete("/api/admin/admins/:id", passport.authenticate("jwt"), async (req, res) => {
+      try {
+        const { id } = req.params;
+        await Users.destroy({ where: { id, admin: true } });
+        res.status(200).send({data: true});
+      } catch (e) {
+        console.log(e);
+      }
+    });
 };
