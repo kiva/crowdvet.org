9efbc099bcd1b7cbc3e94a997c7e81b2d07bd15a
walter <buczacka.walter@gmail.com>
2018-08-31 14:02:00 -0300

    update users components

--- client/src/components/Admin/Users.js
+++ client/src/components/Admin/Users.js
@@ -15,12 +15,26 @@ import {
   ReferenceInput
 } from 'react-admin';
 
+const required = (message = 'Required') =>
+    value => value ? undefined : message;
+
+
 export const UsersList = props => (
   <List {...props}>
     <Datagrid>
       <TextField source="id" />
       <TextField label="name" source="name"/>
+      <TextField label="email" source="email"/>
       <EditButton />
     </Datagrid>
   </List>
+);
+
+export const UsersEdit = props => (
+  <Edit title="Edit" {...props}>
+    <SimpleForm>
+      <TextInput source="name" validate={ required() } />
+      <TextInput label="email" source="email" validate={ required() } />
+    </SimpleForm>
+  </Edit>
 );
